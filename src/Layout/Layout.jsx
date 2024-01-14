519757fe4165418dc28a32cd0444fcb4fdc701c0
MAHBUBUL ALAM <33549330+promahbubul@users.noreply.github.com>
2024-01-15 01:21:25 +0600

    mobile menu

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import Sidebar from "../components/Layout/Sidebar";
 import { Outlet } from "react-router-dom";
 
 const Layout = () => {
+  const [menu, setMenu] = useState(false);
+
+  console.log(menu);
   return (
-    <div className="flex flex-row w-screen">
+    <div className="flex md:flex-row flex-col w-screen">
       {/* Sidebar */}
-      <Sidebar />
+      <Sidebar menu={menu} setMenu={setMenu} />
       {/* Content */}
       <section className="w-full pt-5 px-3 md:px-5 pb-2  md:h-[calc(100vh-44px)] box-border">
         <Outlet />
--- src/components/Layout/Sidebar.jsx
+++ src/components/Layout/Sidebar.jsx
@@ -7,8 +7,10 @@ import {
 import SidebarLinks from "./SidebarLinks";
 import OffButton from "../ui/Button/ToggleButton/OffButton";
 import OnButton from "../ui/Button/ToggleButton/OnButton";
+import { SlMenu } from "react-icons/sl";
+import { CgCloseO } from "react-icons/cg";
 
-const Sidebar = () => {
+const Sidebar = ({ menu, setMenu }) => {
   const [darkMode, setDarkMode] = useState(false);
 
   const toggleTheme = () => {
@@ -20,51 +22,85 @@ const Sidebar = () => {
     return document.documentElement.classList.contains("dark");
   };
   return (
-    <section className="bg-gray-bg-1 hidden md:block md:w-[320px] h-screen py-5 box-border dark:bg-blue-sidebar ">
-      {/* top */}
-      <div className="flex flex-col px-5 gap-10">
-        <div className="flex flex-row  gap-2 items-center">
-          <img src={logo} alt="" className="" />
-          <h3 className="text-primary dark:text-white text-xl font-medium">
-            Lucidmark.
-          </h3>
-        </div>
-        <button className="bg-primary-soft-2 dark:bg-primary dark:text-white text-primary text-sm font-medium leading-normal py-3 rounded-full w-full">
-          New Environment
+    <>
+      {menu ? (
+        ""
+      ) : (
+        <button
+          onClick={() => setMenu(!menu)}
+          className=" md:hidden text-right flex justify-end static
+           mt-5 mr-5"
+        >
+          <SlMenu className="text-2xl" />
         </button>
-      </div>
-      {/* middle */}
-      <div className="h-[calc(100vh-359.122px)] ">
-        <SidebarLinks />
-      </div>
-      {/* bottom */}
-      <div className="px-5 flex flex-col gap-3">
-        <div className="">
-          <button onClick={toggleTheme}>
-            {darkMode ? <OnButton /> : <OffButton />}
+      )}
+      {menu ? (
+        <div
+          onClick={() => setMenu(!menu)}
+          className="fixed top-0 right-0 bottom-0 left-0 bg-black-1/70"
+        ></div>
+      ) : (
+        ""
+      )}
+      <section
+        className={`bg-gray-bg-1 duration-300 transition-transform  md:block md:w-[320px] h-screen py-5 box-border dark:bg-blue-sidebar ${
+          menu ? "fixed w-10/12" : "hidden"
+        }`}
+      >
+        {/* top */}
+        <div className="flex flex-col px-5 gap-10">
+          <div className="flex flex-row justify-between">
+            <div className="flex flex-row  gap-2 items-center">
+              <img src={logo} alt="" className="" />
+              <h3 className="text-primary dark:text-white text-xl font-medium">
+                Lucidmark.
+              </h3>
+            </div>
+            {menu ? (
+              <button onClick={() => setMenu(!menu)} className="">
+                <CgCloseO className="text-2xl text-error" />
+              </button>
+            ) : (
+              ""
+            )}
+          </div>
+          <button className="bg-primary-soft-2 dark:bg-primary dark:text-white text-primary text-sm font-medium leading-normal py-3 rounded-full w-full">
+            New Environment
           </button>
         </div>
-        <div className="flex cursor-pointer flex-row justify-between items-center">
-          <div className="flex flex-row gap-2 items-center">
-            <img src={avatar} alt="" className="" />
-            <h4 className="text-sm text-gray-text-1 dark:text-gray-text-dark font-semibold">
-              Liam Trampota
-            </h4>
-          </div>
-          <IoIosArrowForward className="text-gray-text-1 dark:text-gray-text-dark " />
+        {/* middle */}
+        <div className="h-[calc(100vh-359.122px)] ">
+          <SidebarLinks />
         </div>
-        <div className="flex cursor-pointer flex-row justify-between items-center">
-          <div className="flex flex-row gap-2 items-center">
-            <div className="bg-primary p-2 rounded-full">
-              <IoNotificationsOutline className="text-white text-xl" />
+        {/* bottom */}
+        <div className="px-5 flex flex-col gap-3">
+          <div className="">
+            <button onClick={toggleTheme}>
+              {darkMode ? <OnButton /> : <OffButton />}
+            </button>
+          </div>
+          <div className="flex cursor-pointer flex-row justify-between items-center">
+            <div className="flex flex-row gap-2 items-center">
+              <img src={avatar} alt="" className="" />
+              <h4 className="text-sm text-gray-text-1 dark:text-gray-text-dark font-semibold">
+                Liam Trampota
+              </h4>
             </div>
-            <h4 className="text-sm font-semibold text-gray-text-1 dark:text-gray-text-dark">
-              Notifications
-            </h4>
+            <IoIosArrowForward className="text-gray-text-1 dark:text-gray-text-dark " />
+          </div>
+          <div className="flex cursor-pointer flex-row justify-between items-center">
+            <div className="flex flex-row gap-2 items-center">
+              <div className="bg-primary p-2 rounded-full">
+                <IoNotificationsOutline className="text-white text-xl" />
+              </div>
+              <h4 className="text-sm font-semibold text-gray-text-1 dark:text-gray-text-dark">
+                Notifications
+              </h4>
+            </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 };
 
