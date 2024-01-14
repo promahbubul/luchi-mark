d43c5de06a1bc226f5eaaa31fb3cde24c1d178c2
MAHBUBUL ALAM <33549330+promahbubul@users.noreply.github.com>
2024-01-15 00:43:56 +0600

    update

--- src/components/Layout/SidebarLinks.jsx
+++ src/components/Layout/SidebarLinks.jsx
@@ -27,7 +27,7 @@ const SidebarLinks = () => {
           className={`flex group items-center flex-row mb-1 gap-5 border-l-4 border-l-transparent pl-3 py-3  text-gray-text-1  dark:text-gray-text-dark  font-semibold focus:border-l-4 hover:border-l-4 hover:border-primary focus:border-l-primary   `}
         >
           <item.icons className="text-gray-text-1  dark:text-gray-text-dark  group-hover:text-primary text-xl group-focus:text-primary" />
-          <span className="group-hover:text-primary group-focus:text-primary  ">
+          <span className="group-hover:text-primary text-gray-text-1 group-focus:text-primary  ">
             {item.title}
           </span>
         </NavLink>
